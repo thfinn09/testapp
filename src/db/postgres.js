9e1733378e1c2b15017f465b8dc327a9cb4ed94c
thfinn <hongocthuan25091999@gmail.com>
2025-12-19 16:39:27 +0700

    fix postgres env config

--- src/db/postgres.js
+++ src/db/postgres.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: 'thuan25092003',
-  database: 'todo_db',
-  port: 5432
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool
